8baa3c8f43401b053eaadda60f5ed29eee145ae5
Simon Croome <simon.croome@storageos.com>
2017-02-15 15:15:54 +0000

    PascalCase for rules json

--- types/rule.go
+++ types/rule.go
@@ -19,41 +19,39 @@ type RuleAction string
 // Rule is used to define a rule
 type Rule struct {
 
-	// Volume unique ID.
+	// Rule unique ID.
 	// Read Only: true
 	ID string `json:"ID,omitempty"`
 
 	// Rule name.
 	// Required: true
-	Name string `json:"name"`
+	Name string `json:"Name"`
 
 	// Rule description.
-	Description string `json:"description"`
+	Description string `json:"Description"`
 
-	// Flag describing whether rule is active.
-	// Required: true
+	// Flag describing whether the rule is active.
 	// Default: false
-	Active bool `json:"active"`
+	Active bool `json:"Active"`
 
 	// Weight is used to determine order during rule processing.  Rules with
 	// heavier weights are processed later.
 	// default: 0
-	Weight int `json:"weight"`
+	Weight int `json:"Weight"`
 
 	// Operator is used to compare objects or labels.
-	Operator Operator `json:"operator"`
+	Operator Operator `json:"Operator"`
 
 	// RuleAction controls whether the action is to add or remove a label from the
 	// matching object(s).
-	// default: add
-	RuleAction RuleAction `json:"rule_action"`
+	RuleAction RuleAction `json:"RuleAction"`
 
 	// Selectors defines the list of labels that should trigger a rule.
-	Selectors map[string]string `json:"selectors"`
+	Selectors map[string]string `json:"Selectors"`
 
 	// Labels defines the list of labels that will be added or removed from the
 	// matching object(s).
-	Labels map[string]string `json:"labels"`
+	Labels map[string]string `json:"Labels"`
 }
 
 // Rules is a collection of Rules.
