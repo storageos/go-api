87601ba985bf08669d22079dcccc0d4eecefa9f1
Simon Croome <simon.croome@storageos.com>
2017-02-23 14:42:32 +0000

    Remove swagger autogen

--- hack/generate-swagger-api.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Disabling model generation
-# swagger generate model -f swagger.yaml \
-#     -m types --skip-validator -C swagger-gen.yaml \
-#     -n ErrorResponse \
-#     -n Deployment \
-#     -n Volume
