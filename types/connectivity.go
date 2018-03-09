7bc228b096749e52c3d5b9e1f587690960fbc3f3
Joe Reid <joe.reid@storageos.com>
2018-03-09 12:34:48 +0000

    add etcd and nats fields

--- types/connectivity.go
+++ types/connectivity.go
@@ -3,8 +3,9 @@ package types
 type ConnectivityResult struct {
 	Target *Node `json:"target"`
 
-	APIConnectivity bool `json:"apiConnectivity"`
-	// TODO: fields for testing other services, eg DFS or ETCD
+	APIConnectivity  bool `json:"apiConnectivity"`
+	NatsConnectivity bool `json:"natsConnectivity"`
+	EtcdConnectivity bool `json:"etcdConnectivity"`
 
 	Pass    bool    `json:"pass"`    // logical and of all connectivity tests (eg. API && etcd && DFS etc)
 	Timeout bool    `json:"timeout"` // true iff the test timed out before completion
