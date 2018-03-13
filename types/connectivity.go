7a440eac2a90060558818bcd4689b7221bc1edb5
Simon Croome <simon.croome@storageos.com>
2018-03-13 19:39:00 +0000

    Updated for new result struct

--- types/connectivity.go
+++ types/connectivity.go
@@ -1,13 +1,30 @@
 package types
 
+type TestName string
+
+const (
+	APIConnectivity  TestName = "api"
+	NatsConnectivity TestName = "nats"
+	EtcdConnectivity TestName = "etcd"
+)
+
+// ConnectivityResult capture's a node connectivity report to a given target.
 type ConnectivityResult struct {
 	Target *Node `json:"target"`
 
-	APIConnectivity  bool `json:"apiConnectivity"`
-	NatsConnectivity bool `json:"natsConnectivity"`
-	EtcdConnectivity bool `json:"etcdConnectivity"`
+	Connectivity map[TestName]bool `json:"connectivity"`
 
-	Pass    bool    `json:"pass"`    // logical and of all connectivity tests (eg. API && etcd && DFS etc)
 	Timeout bool    `json:"timeout"` // true iff the test timed out before completion
 	Errors  []error `json:"errors"`  // errors encountered during testing
+}
+
+// Passes returns true iff all tests passed
+func (r ConnectivityResult) Passes() bool {
+	passes := len(r.Connectivity) > 0
+
+	for _, v := range r.Connectivity {
+		passes = passes && v
+	}
+
+	return passes
 }
