44642af910f11b457e45e4823fed3a582b6fd286
Simon Croome <simon.croome@storageos.com>
2020-07-17 10:20:57 +0100

    Bump api to v2.1.0

--- v2/model_resize_volume_request.go
+++ v2/model_resize_volume_request.go
@@ -9,10 +9,10 @@
  */
 
 package api
-// UpdateNodeData struct for UpdateNodeData
-type UpdateNodeData struct {
-	// A set of arbitrary key value labels to apply to the entity. 
-	Labels map[string]string `json:"labels,omitempty"`
+// ResizeVolumeRequest struct for ResizeVolumeRequest
+type ResizeVolumeRequest struct {
+	// The desired new size for the volume in  bytes. This value cannot be less than  the current size of the volume. 
+	SizeBytes uint64 `json:"sizeBytes,omitempty"`
 	// An opaque representation of an entity version at the time it was obtained from the API. All operations that mutate the entity must include this version field in the request unchanged. The format of this type is undefined and may change but the defined properties will not change. 
 	Version string `json:"version,omitempty"`
 }
